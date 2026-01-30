bc041b9ac1c859cd4768f304f536ccb04e5b2138
Tomasz Zok <tomasz.zok@gmail.com>
2026-01-30 11:18:04 +0100

    build: Update shebang in build-docker.sh and add .aider* to gitignore

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /usr/bin/env bash
 docker build \
 	--tag onquadro-frontend:$(git describe --all --tags --contains | tr -c '[0-9A-Za-z_.]' - | tr -s - | sed -e 's/^-//g' -e 's/-$//g') \
 	--tag onquadro-frontend:latest \
